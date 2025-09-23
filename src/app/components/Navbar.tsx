116daa42c021760ce833fe6cd5654700f7345f86
Isa <isachef13@gmail.com>
2025-09-23 11:55:23 -0600

    Actualizacion navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,41 +1,37 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Navbar() {
   return (
     <nav className="sticky top-0 left-0 z-50 w-full bg-F1F1F1 text-black md:px-8 py-4">
       <div className="w-full flex justify-between items-center md:px-8 py-2">
         {/* Left Section with 3 Links */}
-        <section className="flex space-x-4">
+        <section className="flex space-x-20">
+          <Image src="/logoEjemplo.png" alt="Tu Logo"
+          height={208} width={213}
+          style={{ borderRadius: '50%', objectFit: 'cover' }}>
+          </Image>
           <Link href="/link1" className="hover:underline">
-            Link 1
+            Inicio
           </Link>
           <Link href="/link2" className="hover:underline">
-            Link 2
+            Preguntas Frecuentes
           </Link>
           <Link href="/link3" className="hover:underline">
-            Link 3
+            Contacto
+          </Link>
+           <Link href="/link3" className="hover:underline">
+            Métodos de Pago 
+          </Link>
+           <Link href="/link3" className="hover:underline">
+            Comprar Boletos
           </Link>
         </section>
 
         {/* Middle Section with Icon */}
-        <section className="flex justify-center">
-          <Link href="/" className="text-lg font-semibold hover:underline">
-            ICON
-          </Link>
-        </section>
+        
 
-        {/* Right Section with 3 Links */}
-        <section className="flex space-x-4">
-          <Link href="/link4" className="hover:underline">
-            Link 4
-          </Link>
-          <Link href="/link5" className="hover:underline">
-            Link 5
-          </Link>
-          <Link href="/link6" className="hover:underline">
-            Link 6
-          </Link>
-        </section>
+       
       </div>
     </nav>
   );
